3340dc3a13f5ed4e43859055f49abe2fa47e51e1
T. Hamilton <profhamilton3@gmail.com>
2024-08-29 17:56:18 -0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+def doDriveForward(num: number, num2: number):
+    DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.GREEN)
+    DFRobotMaqueenPlus.motot_run(Motors.ALL, Dir.CW, num)
+    basic.pause(num2)
+    DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.OFF)
+
 def on_button_pressed_a():
     music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
         music.PlaybackMode.UNTIL_DONE)
@@ -6,6 +12,8 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 def on_button_pressed_b():
     DFRobotMaqueenPlus.i2c_init()
     DFRobotMaqueenPlus.servo_run(Servos.S1, 45)
+    doDriveForward(50,100)
+
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 basic.show_icon(IconNames.SAD)
--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
+function doDriveForward (num: number, num2: number) {
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.GREEN)
+    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, num)
+    basic.pause(num2)
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
+}
 input.onButtonPressed(Button.A, function () {
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
 })
 input.onButtonPressed(Button.B, function () {
     DFRobotMaqueenPlus.I2CInit()
     DFRobotMaqueenPlus.servoRun(Servos.S1, 45)
+    doDriveForward(50, 100)
+    doDriveBackward(35, 150)
 })
+function doDriveBackward (num: number, num2: number) {
+	
+}
 basic.showIcon(IconNames.Sad)
